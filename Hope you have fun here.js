6ae974fcf44d27c10ee60614fd4b44bab4f328ab
zhaozhixin810 <79979131+zhaozhixin810@users.noreply.github.com>
2021-04-14 19:36:29 +0800

    homework2

---
+++ Hope you have fun here.js
@@ -0,0 +1,55 @@
+function sayHello(){
+var response = prompt("What is your name?");
+alert("Hello " + response + ", have a good day! Let's try something interesting!");
+}
+
+function question1(){
+  var response1=prompt("what is the Ultraman?(Please answer in Chinese and note the full name of it)");
+  if(response1==="戴拿奥特曼"){
+  alert("Do a good job！It's 戴拿奥特曼!");
+  }
+  else{
+  alert("Sorry,it's pity that you got a wrong answer. You can check up the tip.")
+  }
+}
+
+
+function question2(){
+  var response2=prompt("what is the Ultraman?(Please answer in Chinese and note the full name of it)");
+  if(response2==="奈克赛斯奥特曼"){
+  alert("Do a good job！It's 奈克赛斯奥特曼!");
+  }
+  else{
+  alert("Sorry,it's pity that you got a wrong answer. You can check up the tip.")
+  }
+}
+
+function question3(){
+  var response3=prompt("what is the Ultraman?(Please answer in Chinese and note the full name of it)");
+  if(response3==="奥特之母"){
+  alert("Do a good job！It's 奥特之母!");
+  }
+  else{
+  alert("Sorry,it's pity that you got a wrong answer. You can check up the tip.")
+  }
+}
+
+function question4(){
+ alert("He is 赛罗奥特曼.I can't believe you think he's the most handsome. Well, I respect your decision. But there must be a better answer.");
+}
+
+function question5(){
+  alert("He is 银河奥特曼.I can't believe you think he's the most handsome, well, I respect your decision, but there must be a better answer.");
+}
+
+function question6(){
+  alert("Yes,you are very discerning!迪迦奥特曼yyds！")
+}
+
+function question7(){
+  alert("He is 盖亚奥特曼.I can't believe you think he's the most handsome, well, I respect your decision, but there must be a better answer.")
+}
+
+function question8(){
+alert("Please choose the most hansome Ultraman below.");
+}
